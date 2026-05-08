0bd3ea77206650fd60b5441e4a5cc463f45cfcf8
KING DULA 2M <dulanthikshana69@gmail.com>
2026-05-08 16:49:21 +0530

    Update config.js

--- config.js
+++ config.js
@@ -8,7 +8,7 @@ module.exports = {
 SESSION_ID: process.env.SESSION_ID || "",
 ALIVE_IMG: process.env.ALIVE_IMG || "https://raw.githubusercontent.com/dulanthikshana69-sketch/-KING-DULA-2M-BOT/refs/heads/main/images/King%20Dula%2C%20gaming%20avatar%20king.png",
 ALIVE_MSG: process.env.ALIVE_MSG || "*Hello👋 KING DULA BOT 😈*",
-BOT_OWNER: '94776121326',  // Replace with the owner's phone number
+BOT_OWNER: '94787535242',  // Replace with the owner's phone number
 
 
 
