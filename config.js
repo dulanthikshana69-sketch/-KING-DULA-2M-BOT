75c698bd802b2184022c21c848cb990e9faa8ab9
KING DULA 2M <dulanthikshana69@gmail.com>
2026-05-08 16:47:41 +0530

    Update ALIVE_IMG and ALIVE_MSG in config.js

--- config.js
+++ config.js
@@ -6,8 +6,8 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 SESSION_ID: process.env.SESSION_ID || "",
-ALIVE_IMG: process.env.ALIVE_IMG || "https://github.com/DANUWA-MD/DANUWA-MD/blob/main/images/DANUWA-MD.png?raw=true",
-ALIVE_MSG: process.env.ALIVE_MSG || "*Hello👋 DANUWA-MD Is Alive Now😍*",
+ALIVE_IMG: process.env.ALIVE_IMG || "https://raw.githubusercontent.com/dulanthikshana69-sketch/-KING-DULA-2M-BOT/refs/heads/main/images/King%20Dula%2C%20gaming%20avatar%20king.png",
+ALIVE_MSG: process.env.ALIVE_MSG || "*Hello👋 KING DULA BOT 😈*",
 BOT_OWNER: '94776121326',  // Replace with the owner's phone number
 
 
